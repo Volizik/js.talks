76fbb2691bb4cb80337f396ed0da2ecb7d411c2f
Kizilov <kizilov.yura@gmail.com>
2022-05-15 17:14:41 +0300

    added js weird

--- jsIsWeirdReturnA/jsIsWeirdReturnA1.js
+++ jsIsWeirdReturnA/jsIsWeirdReturnA1.js
@@ -1,10 +1,10 @@
 // Return "a" using only characters below:
 // +()[]=!
 
-// ([]+([]==[])) returns "false"
+// ([]+([]==[])) becomes "false"
 //      because [] == []  returns false
 //      and [] + something returns this something as a string
-// [+(!![])] returns [1]
+// [+(!![])] becomes [1]
 //      (!![]) becomes true because empty array it's an object, and convert object to bool (!!) becomes true
 //      + before bool convert it to 0 (false) or to 1 (true)
 const jsIsWeirdReturnA = () => ([]+([]==[]))[+(!![])]
